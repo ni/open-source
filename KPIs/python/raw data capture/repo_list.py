f4d5306e66b2b56a1d4ecfb5f13b3532cf4211be
sergiov-ni <sergio.velderrain@ni.com>
2025-01-13 16:12:41 -0800

    new config

--- KPIs/python/raw data capture/repo_list.py
+++ KPIs/python/raw data capture/repo_list.py
@@ -10,66 +10,66 @@ Each repo dict has:
 
 repo_list = [
     {
-        "enabled": False,
+        "enabled": True,
         "owner": "ni",
         "repo": "grpc-labview",
         "start_date": "2020-11-02",
-        "end_date": ""
+        "end_date": "2021-11-02"
     },
     {
-        "enabled": False,
+        "enabled": True,
         "owner": "ni",
         "repo": "labview-memory-management-tools",
         "start_date": "2018-06-30",
-        "end_date": ""
+        "end_date": "2019-06-30"
     },
     {
-        "enabled": False,
+        "enabled": True,
         "owner": "ni",
         "repo": "niveristand-custom-device-development-tools",
         "start_date": "2016-07-13",
-        "end_date": ""
+        "end_date": "2017-07-14"
     },
     {
-        "enabled": False,
+        "enabled": True,
         "owner": "ni",
         "repo": "niveristand-custom-device-build-tools",
         "start_date": "2017-06-14",
-        "end_date": ""
+        "end_date": "2018-06-15"
     },
     {
-        "enabled": False,
+        "enabled": True,
         "owner": "ni",
         "repo": "labview-icon-editor",
-        "start_date": "2024-01-01",
-        "end_date": ""
+        "start_date": "2024-05-01",
+        "end_date": "2025-05-02"
     },
     {
         "enabled": True,
         "owner": "ni",
         "repo": "actor-framework",
         "start_date": "2024-08-15",
-        "end_date": ""
+        "end_date": "2025-08-16"
     },
     {
         "enabled": True,
         "owner": "facebook",
         "repo": "react",
         "start_date": "2013-06-03",
-        "end_date": "2014-06-03"
+        "end_date": "2014-06-04"
     },
     {
         "enabled": True,
         "owner": "tensorflow",
         "repo": "tensorflow",
         "start_date": "2015-11-09",
-        "end_date": "2016-11-09"
+        "end_date": "2016-11-10"
     },
     {
         "enabled": True,
         "owner": "dotnet",
         "repo": "core",
         "start_date": "2014-11-12",
-        "end_date": "2015-11-12"
+        "end_date": "2015-11-13"
     }
 ]
